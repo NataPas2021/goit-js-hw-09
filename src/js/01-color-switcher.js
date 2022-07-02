b4cd01e4ac3cea19900e074a74f06509a347a089
NataPas2021 <natapas13@gmail.com>
2022-07-03 02:19:58 +0300

    task 1 ready + calendar lib init

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    bodyColor: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
 
+const INTERVAL_DELAY = 1000;
+let backgroundBodyColor = undefined; 
+
+refs.startBtn.addEventListener('click', onClickColorChange);
+refs.stopBtn.addEventListener('click', onClickStopChangingColor);
+
+function onClickColorChange(e) {
+    backgroundBodyColorChange = setInterval(changeBackgroundColor, INTERVAL_DELAY, changeBackgroundColor);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+}
+
+function onClickStopChangingColor(e) {
+  clearInterval(backgroundBodyColorChange, INTERVAL_DELAY);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
+
+function changeBackgroundColor() {
+  refs.bodyColor.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
