375cb4b4a54c5bb5f644ab7b34b44ee428428de6
NataPas2021 <natapas13@gmail.com>
2022-07-06 02:32:21 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from "notiflix";
+
+const formRef = document.querySelector('form');
+  
+
+
+formRef.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise ((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay)
+    
+  })
+  
 }
+
+function onFormSubmit (e) {
+  e.preventDefault();
+  const { delay, step, amount } = event.target.elements;
+  let delayStep = Number(delay.value);
+  for(i = 1; i <= amount.value; i+=1) {
+     createPromise(i, delayStep).then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    delayStep += Number(step.value);
+    
+  }
+  formRef.reset();
+}
+
+
+
+
