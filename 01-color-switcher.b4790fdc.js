24b1c6061bacd1376324914b130910845ea1396a
NataPas2021 <94239256+NataPas2021@users.noreply.github.com>
2022-07-06 09:29:11 +0000

    Deploying to gh-pages from @ NataPas2021/goit-js-hw-09@a77529b5ce07bfc8cc09a8a8a411e5f8ae2d102f 🚀

---
+++ 01-color-switcher.b4790fdc.js
@@ -0,0 +1,2 @@
+!function(){const t={bodyColor:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e;function o(){t.bodyColor.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.startBtn.addEventListener("click",(function(n){e=setInterval(o,1e3,o),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(o){clearInterval(e,1e3),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.b4790fdc.js.map
